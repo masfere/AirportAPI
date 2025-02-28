2371285a10a7c43ee721ef3a99e7d18833841755
MaFê <mafe@email.com>
2025-02-28 09:42:00 -0300

    Término a Fase 2 Implementado endpoint Airport Add interface AirportRepository Add classe AirportService Add classe AirportController

---
+++ src/main/java/br/dev/mafe/airports/controllers/AirportController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.dev.mafe.airports.controllers;
+
+import br.dev.mafe.airports.entities.Airport;
+import br.dev.mafe.airports.services.AirportService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author sesidevb
+ */
+@RestController
+public class AirportController {
+    
+    @Autowired
+    private AirportService airportService;
+    
+    @GetMapping("/airport")
+    public List<Airport> findAll() {
+        List<Airport> result = airportService.findAll();
+        return result;
+    }
+    
+}
---
+++ src/main/java/br/dev/mafe/airports/services/AirportService.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.dev.mafe.airports.services;
+
+import br.dev.mafe.airports.entities.Airport;
+import br.dev.mafe.airports.repositories.AirportRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author sesidevb
+ */
+@Service
+public class AirportService {
+    
+    @Autowired
+    private AirportRepository airportRepository;
+    
+    public List<Airport> findAll() {
+        
+        List<Airport> result = airportRepository.findAll();
+        return result;
+    }
+}
