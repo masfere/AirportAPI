4f4bdd0db3dfca609ff08b9af4cbfc19a52ea783
MaFê <mafe@email.com>
2025-03-21 09:54:22 -0300

    Término da Fase Update interface AirportRepository - Adicionado assinatura de método Airport findByIataCode(). Update classe AirportService -Adicionado método findByIataCodeSearch. Update classe AirportController -Adicionado endpoint iatacode/{iataCode}.

--- src/main/java/br/dev/mafe/airports/controllers/AirportController.java
+++ src/main/java/br/dev/mafe/airports/controllers/AirportController.java
@@ -60,6 +60,17 @@ public class AirportController {
             return ResponseEntity.ok(result);
         }
     }
+    @GetMapping("/iatacode/{iataCode}")
+    public ResponseEntity<Airport> findByIataCode(@PathVariable String iataCode){
+        Airport result = airportService.findByIataCode(iataCode);
+        
+        if (result == null) {
+            return ResponseEntity.notFound().build();
+        
+        }else {
+            return ResponseEntity.ok(result);
+        }
+    }
 
     public AirportService getAirportService() {
         return airportService;
--- src/main/java/br/dev/mafe/airports/services/AirportService.java
+++ src/main/java/br/dev/mafe/airports/services/AirportService.java
@@ -45,5 +45,10 @@ public class AirportService {
         
         return resultDTO;
     }
+    
+    public Airport findByIataCode(String iataCode) {
+        Airport result = airportRepository.findByIataCode(iataCode);
+        return result;
+    }
      
 }
